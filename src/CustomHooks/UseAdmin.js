daff8f756dc32f3c8ec2fb5213682df611203f51
Shagor <mdshagormahamud028@gmail.com>
2022-12-22 00:21:27 +0600

    cheak the admin route

--- src/CustomHooks/UseAdmin.js
+++ src/CustomHooks/UseAdmin.js
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
 
-const UseAdmin = (email) => {
-    const [admin, setAdmin] = useState(false)
-    const [adLoading, setAdLoading] = useState(true)
+// const UseAdmin = (email) => {
+//     const [admin, setAdmin] = useState(false)
+//     const [adLoading, setAdLoading] = useState(true)
 
-    useEffect(() => {
-        if (email) {
-            fetch(`https://used-product-laptop-market-server.vercel.app/users/admin/${email}`)
-                .then(res => res.json())
-                .then(data => {
-                    setAdmin(data.isAdmin);
-                    setAdLoading(false);
-                })
-        }
-    }, [email])
+//     useEffect(() => {
+//         if (email) {
+//             fetch(`https://used-product-laptop-market-server.vercel.app/users/admin/${email}`)
+//                 .then(res => res.json())
+//                 .then(data => {
+//                     setAdmin(data.isAdmin);
+//                     setAdLoading(false);
+//                 })
+//         }
+//     }, [email])
 
-    return [admin, adLoading]
-}
+//     return [admin, adLoading]
+// }
 
-export default UseAdmin;
+// export default UseAdmin;
--- src/CustomHooks/UseSeller.js
+++ src/CustomHooks/UseSeller.js
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react"
+// import { useEffect, useState } from "react"
 
-const UseSeller = (email) => {
-    const [seller, setSeller] = useState(false)
-    const [sellerLoading, setSellerLoading] = useState(true)
+// const UseSeller = (email) => {
+//     const [seller, setSeller] = useState(false)
+//     const [sellerLoading, setSellerLoading] = useState(true)
 
-    useEffect(() => {
-        if (email) {
-            fetch(`https://used-product-laptop-market-server.vercel.app/users/seller/${email}`)
-                .then(res => res.json())
-                .then(data => {
-                    setSeller(data.isSeller);
-                    setSellerLoading(false);
-                })
-        }
-    }, [email])
+//     useEffect(() => {
+//         if (email) {
+//             fetch(`https://used-product-laptop-market-server.vercel.app/users/seller/${email}`)
+//                 .then(res => res.json())
+//                 .then(data => {
+//                     setSeller(data.isSeller);
+//                     setSellerLoading(false);
+//                 })
+//         }
+//     }, [email])
 
-    return [seller, sellerLoading]
-};
+//     return [seller, sellerLoading]
+// };
 
-export default UseSeller
+// export default UseSeller
